50a3ae0ba1b4a715341b50d2a41bb30fe46e94ea
mine0117 <eagleeye0117@naver.com>
2021-01-03 21:42:31 +0900

    21-01-03

--- Baekjoon/Baekjoon/src/January/Main_1931_회의실배정.java
+++ Baekjoon/Baekjoon/src/January/Main_1931_회의실배정.java
@@ -5,12 +5,18 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main_1931_회의실배정 {
-
+    
+	
+	// 1. 일단 이 문제는 이전 선택의 종료시간과 이후 선택의 시작시간이 겹치면 안됨
+	// 2. 최대한 많은활동을 선택할려면 종료시간이 짧아야함
+	// 3. 내가 푼 방법 : 종료시간을 기준으로 정렬하고, 이전 종료시간에 대해 겹치는것을 제외하고 나머지 부분에서 선택하여 풀어봄
+	// 4. 후기 : Comparator을 사용을 거의 안해봐서 고생했음, 다른 방법도 있을것같은데 모르겠다.
+	
 	static int N;
 	static int[][] arr;
 
 	public static void main(String[] args) {
-
+        
 		Scanner sc = new Scanner(System.in);
 
 		N = sc.nextInt();
