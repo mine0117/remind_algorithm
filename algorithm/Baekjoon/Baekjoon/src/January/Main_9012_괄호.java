b714f202e064793fc4ca5014c60f9d5bf73b9781
mine0117 <eagleeye0117@naver.com>
2021-01-05 21:17:37 +0900

    21-01-05

--- algorithm/Baekjoon/Baekjoon/src/January/Main_9012_괄호.java
+++ algorithm/Baekjoon/Baekjoon/src/January/Main_9012_괄호.java
@@ -1,10 +1,14 @@
 package January;
 
+import java.util.Scanner;
+
 public class Main_9012_°ýÈ£ {
 
 	public static void main(String[] args) {
-	
 
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
 	}
 
 }
