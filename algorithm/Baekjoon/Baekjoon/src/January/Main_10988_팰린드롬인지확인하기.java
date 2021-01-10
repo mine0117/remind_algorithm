47728e3ad798b66c6a9c3eebd947b3788cd43e3d
mine0117 <eagleeye0117@naver.com>
2021-01-10 10:33:11 +0900

    21-01-10

--- algorithm/Baekjoon/Baekjoon/src/January/Main_10988_팰린드롬인지확인하기.java
+++ algorithm/Baekjoon/Baekjoon/src/January/Main_10988_팰린드롬인지확인하기.java
@@ -7,6 +7,21 @@ public class Main_10988_팰린드롬인지확인하기 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+
+		String str = sc.next();
+
+		char[] arr = new char[str.length()];
+
+		for (int i = 0; i < str.length(); i++) {
+			arr[i] = str.charAt(i);
+		}
+		int ans = 1;
+		for (int i = 0; i < arr.length / 2; i++) {
+			if (arr[i] != arr[arr.length - i - 1]) {
+				 ans = 0;
+			}
+		}
+		System.out.println(ans);
 	}
 
 }
