a05295613f8ebddbaf29b5bf8f140bbf17ea63d7
mine0117 <eagleeye0117@naver.com>
2021-01-14 20:46:27 +0900

    21-01-14

--- algorithm/Baekjoon/Baekjoon/src/January/Main_15684_사다리조작.java
+++ algorithm/Baekjoon/Baekjoon/src/January/Main_15684_사다리조작.java
@@ -1,9 +1,15 @@
 package January;
 
+import java.util.Scanner;
+
 public class Main_15684_사다리조작 {
 
+	static int N, M, H;
+
 	public static void main(String[] args) {
-	
+
+		Scanner sc = new Scanner(System.in);
+		
 		
 	}
 
