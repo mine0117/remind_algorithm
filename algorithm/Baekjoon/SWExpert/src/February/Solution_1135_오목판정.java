713622e1d62c50b9edfc210eddda92e9e6a68639
mine0117 <eagleeye0117@naver.com>
2021-02-06 20:42:00 +0900

    21-02-06

--- algorithm/Baekjoon/SWExpert/src/February/Solution_1135_오목판정.java
+++ algorithm/Baekjoon/SWExpert/src/February/Solution_1135_오목판정.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class Solution_1135_오목판정 {
 
+	static char[][] map;
+
 	public static void main(String[] args) {
-	   
+
 		Scanner sc = new Scanner(System.in);
-	
+
 		int T = sc.nextInt();
 		int N = sc.nextInt();
-		
+
 	}
 
 }
