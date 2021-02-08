7e331546a441a93c4c9268f216bbe9caec0efa01
mine0117 <eagleeye0117@naver.com>
2021-02-08 23:04:25 +0900

    21-02-08

--- algorithm/Baekjoon/SWExpert/src/February/SWEA_5948_새샘이의735게임.java
+++ algorithm/Baekjoon/SWExpert/src/February/SWEA_5948_새샘이의735게임.java
@@ -1,10 +1,12 @@
 package February;
 
+import java.util.Scanner;
+
 public class SWEA_5948_새샘이의735게임 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
 	}
 
 }
