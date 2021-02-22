42e4c0fa2c15fe7da2e36e770c3ac1b905b33b1d
mine0117 <eagleeye0117@naver.com>
2021-02-22 23:27:20 +0900

    21-02-22

--- algorithm/Baekjoon/SWExpert/src/February/SWEA_등산로조성.java
+++ algorithm/Baekjoon/SWExpert/src/February/SWEA_등산로조성.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class SWEA_등산로조성 {
 
+	static int[][] map;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-	
+
 		int T = sc.nextInt();
-		
+
 		for (int test_case = 1; test_case <= T; test_case++) {
-			
+
 		}
 	}
 }
